b7a41ecc0951970e85d8982e8aed12f1fa9e776e
Adnan Podgora <podgoraadnan@gmail.com>
2023-08-27 17:51:27 +0000

    Add multiplycation game code  .

--- assets/css/images/js/script.js
+++ assets/css/images/js/script.js
@@ -22,6 +22,8 @@ function runGame (gameType){
 
     if (gameType === "addition") {
         displayAditionQwestion (num1 , num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQwestion (num1 , num2)
     } else {
         alert ("Unknown game type $ {gameType}");
         throw "Unknown game type ${gameType}. Aborting!";
@@ -53,6 +55,8 @@ function calculateCorrectAnswer () {
 
     if (operator === "+"){
         return [operand1 + operand2, "addition" ];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert("Unimplemented operator ${operator}");
         throw "Unimplemented operator ${operator}. Aborting!";
@@ -83,6 +87,10 @@ function displaySubtractQwestion () {
 
 }
 
-function displayMultiplyQwestion () {
+function displayMultiplyQwestion (operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 
 }
